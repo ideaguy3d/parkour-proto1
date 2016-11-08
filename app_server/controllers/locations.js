670f46fc13ffe2ce171a297b9717cd08f573ea70
Julius Alvarado <angularjs.uiux@gmail.com>
2016-11-07 18:13:47 -0800

    moved data from the view to the controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -2,17 +2,64 @@
  * Created by Julius Alvarado on 10/30/2016.
  */
 
-module.exports.homelist = function(req, res){
-    res.render('index', {title: 'Hardcore Parkore web app'});
+module.exports.homelist = function (req, res) {
+    res.render('index', {
+        title: 'Hardcore Parkore web app',
+        pageHeader: {
+            title: 'HardcoreParkore',
+            strapline: 'Find locations near you to do some Parkour!'
+        }
+    });
 };
 
-module.exports.locationInfo = function(req, res){
-    res.render('location-info', {title: 'Location info for parkour spots'});
+module.exports.locationInfo = function (req, res) {
+    res.render('location-info', {
+            title: 'Location info for parkour spot.',
+            pageHeader: {title: 'HardcoreParkore.com'},
+            sidebar: {
+                context: 'Parkour is a new sport that uses techniques from gymnastics and acrobatics to own street street structures by doing tricks off them.',
+                callToAction: 'If you\'ve been to this parkour spot and would like to write what you liked or didn\'t like about this spot... Leave a review !  '
+            },
+            location: {
+                name: 'HardcoreParkore',
+                address: '125 Jump Street, San Jose, ca 95112',
+                rating: 3,
+                facilities: ['Stair Sets', 'Gaps', 'Parallel Walls'],
+                coords: {lat: 37.774929, lng: -122.419416},
+                openingTimes: [{
+                    days: 'Monday - Friday',
+                    opening: '7:00am',
+                    closing: '7:00pm',
+                    closed: false
+                },{
+                    days: 'Saturday',
+                    opening: '8:00am',
+                    closing: '5:00pm',
+                    closed: false
+                },{
+                    days: 'Sunday',
+                    closed: true
+                }],
+                reviews: [{
+                    author: 'Romulus Augustus',
+                    rating: 5,
+                    timestamp: '16 July 2013',
+                    reviewText: 'What a great place. I can\'t say enough good things about it.'
+                },{
+                    author: 'Julius Maximus',
+                    rating: 3,
+                    timestamp: '16 June 2013',
+                    reviewText: 'It was okay. Freeform wasn\'t great, but the Phalanx jumping was fast.'
+                }]
+            }
+        }
+    );
 
 };
 
-module.exports.addReview = function(req, res){
-    res.render('location-review-form', {title: 'Review this parkour location'});
+module.exports.addReview = function (req, res) {
+    res.render('location-review-form',
+        {title: 'Review this parkour location'});
 
 };
 
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -2,12 +2,41 @@
  * Created by Julius Alvarado on 11/1/2016.
  */
 
-module.exports.about = function(req, res){
-    res.render('generic-text', {title: 'About Hardcore Parkour'});
+module.exports.about = function (req, res) {
+    res.render('generic-text',
+        {title: 'About Hardcore Parkour'});
 };
 
-module.exports.parkour = function(req, res){
-    res.render('locations-list', {title: 'The Sport of Parkour'});
+module.exports.parkour = function (req, res) {
+    res.render('locations-list', {
+        title: 'The Sport of Parkour',
+        pageHeader: {
+            title: 'HardcoreParkore',
+            strapline: ' Find locations near you to do some Parkour!'
+
+        },
+        sidebar: "Ello, Looking for some good spots to do some parkour? HardcoreParkore.com helps finds the best places to do some parkour.",
+        locations: [
+            {
+                name: 'Freerunning Palace',
+                address: '125 Jump Street, Silicon Valleys, ca 95112',
+                rating: 3,
+                facilities: ['Stair Sets', 'Gaps', 'Parallel Walls'],
+                distance: '100m'
+            }, {
+                name: 'Freeform Jungle',
+                address: '125 Freeform Street, Silicon Valleys, ca 95112',
+                rating: 4,
+                facilities: ['Stair Sets', 'Gaps'],
+                distance: '200m'
+            }, {
+                name: 'Phalanx Jumpers Playground',
+                address: '125 Backflip Street, Silicon Valleys, ca 95112',
+                rating: 2,
+                facilities: ['Stair Sets', 'Gaps', 'Parallel Walls'],
+                distance: '250m'
+            }]
+    });
 };
 
 //
