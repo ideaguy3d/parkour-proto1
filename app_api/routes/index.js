41f66ff9394a9496af3917089aed0aae292c51d5
Julius Alvarado <angularjs.uiux@gmail.com>
2017-04-25 08:40:53 -0700

    quick check

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -9,17 +9,17 @@ var ctrlReviews = require('../controllers/reviews.api.ctrl');
 //routing locations
 
 
-// router.get('/locations', ctrlLocations.locationsListByDistance);
-// router.post('/locations', ctrlLocations.locationsCreate);
-// router.get('/locations/:locationsid', ctrlLocations.locationsReadOne);
-// router.put('/locations/:locationsid', ctrlLocations.locationsUpdateOne);
-// router.delete('/locations/:locationsid', ctrlLocations.locationsDeleteOne);
-//
-// //routing reviews
-// router.post('/locations/:locationid/reviews', ctrlReviews.reviewsCreate);
-// router.get('/locations/:locationid/reviews/:reviewsid', ctrlReviews.reviewsReadOne);
-//
-// module.exports = router;
+router.get('/locations', ctrlLocations.locationsListByDistance);
+router.post('/locations', ctrlLocations.locationsCreate);
+router.get('/locations/:locationsid', ctrlLocations.locationsReadOne);
+router.put('/locations/:locationsid', ctrlLocations.locationsUpdateOne);
+router.delete('/locations/:locationsid', ctrlLocations.locationsDeleteOne);
+
+//routing reviews
+router.post('/locations/:locationid/reviews', ctrlReviews.reviewsCreate);
+router.get('/locations/:locationid/reviews/:reviewsid', ctrlReviews.reviewsReadOne);
+
+module.exports = router;
 
 
 
