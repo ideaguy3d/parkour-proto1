df70e1769e1d81f89fa1aefc995c3ccd41b4f5f5
Julius Alvarado <angularjs.uiux@gmail.com>
2017-07-12 13:31:55 -0700

    finished initial hash table

--- app_server/routes/hashTable.js
+++ app_server/routes/hashTable.js
@@ -1,7 +1,7 @@
 /**
  * Created by Julius Alvarado on 7/12/2017.
  */
-
+var pl = " ->    ";
 function dash() {
     // helper to make results in terminal window more readable
     console.log();
@@ -11,13 +11,44 @@ function dash() {
 
 }
 function HashTableClass() {
+    var table = [];
 
+    this.put = function (key, value) {
+        var pos = loseloseHashCode(key);
+        // console.log(' ->  ' + pos + ' - ' + key);
+        table[pos] = value;
+    };
+
+    this.remove = function (key) {
+        table[loseloseHashCode(key)] = undefined;
+    };
+
+    this.get = function (key) {
+        return table[loseloseHashCode(key)];
+    };
+
+    var loseloseHashCode = function (key) {
+        var hash = 0;
+        for (var i = 0; i < key.length; i++) {
+            hash += key.charCodeAt(i);
+        }
+        return hash % 37;
+    }
 }
 
 dash();
 console.log("in the 'hasTable.js' file:");
 console.log();
-function hashTableTest(){
-
+function hashTableTest() {
+    var hash = new HashTableClass();
+    hash.put('foo', 'felicia@mail.com');
+    hash.put('car', 'carina@mail.com');
+    hash.put('voo', 'vanessa@mail.com');
+    hash.put('jar', 'jessica@mail.com');
+    console.log(pl+hash.get('car'));
+    console.log(pl+hash.get('moo'));
+    hash.remove('jar');
+    console.log(pl+hash.get('jar'));
 }
+hashTableTest();
 dash();
