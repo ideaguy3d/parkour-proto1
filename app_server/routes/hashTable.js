5afb115082dc2ae54dbc69ddf6937a9d23d3c3b1
Julius Alvarado <angularjs.uiux@gmail.com>
2017-07-13 22:54:31 -0700

    finished .get linear probing method for hash table

--- app_server/routes/hashTable.js
+++ app_server/routes/hashTable.js
@@ -13,10 +13,25 @@ function dash() {
 function HashTableClass() {
     var table = [];
 
+    var ValuePairStruct = function (key, value) {
+        this.key = key;
+        this.value = value;
+        this.toString = function () {
+            return '[' + this.key + '-' + this.value + ']';
+        }
+    };
+
     this.put = function (key, value) {
         var pos = loseloseHashCode(key);
-        // console.log(' ->  ' + pos + ' - ' + key);
-        table[pos] = value;
+        if (table[pos] === undefined) {
+            table[pos] = new ValuePairStruct(key, value);
+        } else {
+            var index = ++pos;
+            while (table[index] !== undefined) {
+                ++index;
+            }
+            table[index] = new ValuePairStruct(key, value);
+        }
     };
 
     this.remove = function (key) {
@@ -24,7 +39,27 @@ function HashTableClass() {
     };
 
     this.get = function (key) {
-        return table[loseloseHashCode(key)];
+        var pos = loseloseHashCode(key);
+        if (table[pos] !== undefined) {
+            if (table[pos].key === key) return table[pos].value;
+            else {
+                var index = ++pos, infinieLoopGaurd=0;
+                console.log("above loop index = "+index);
+                console.log(); console.log(table[index]); console.log();
+                while (table[index] === undefined || table[index].key !== key) {
+                    index++;
+                    infinieLoopGaurd++;
+                    if(infinieLoopGaurd > 10000000) {
+                        console.log(table[16]);
+                        console.log("index = "+index); console.log();
+                        break;
+                    }
+                }
+                console.log("after while loop, infinieLoopGaurd = "+infinieLoopGaurd);
+                if (table[index].key === key) return table[index].value;
+            }
+        }
+        return undefined;
     };
 
     var loseloseHashCode = function (key) {
@@ -45,10 +80,34 @@ function hashTableTest() {
     hash.put('car', 'carina@mail.com');
     hash.put('voo', 'vanessa@mail.com');
     hash.put('jar', 'jessica@mail.com');
-    console.log(pl+hash.get('car'));
-    console.log(pl+hash.get('moo'));
+    console.log(pl + hash.get('car'));
+    console.log(pl + hash.get('moo'));
     hash.remove('jar');
-    console.log(pl+hash.get('jar'));
+    console.log(pl + hash.get('jar'));
 }
-hashTableTest();
+function testHash2() {
+    var hash = new HashTableClass();
+    hash.put(' Gandalf', 'gandalf@ email.com');
+    hash.put(' John', 'johnsnow@ email.com');
+    hash.put(' Tyrion', 'tyrion@ email.com');
+    hash.put(' Aaron', 'aaron@ email.com');
+    hash.put(' Donnie', 'donnie@ email.com');
+    hash.put(' Ana', 'ana@ email.com');
+    hash.put(' Jonathan', 'jonathan@ email.com');
+    hash.put(' Jamie', 'jamie@ email.com');
+    hash.put(' Sue', 'sue@ email.com');
+    hash.put(' Mindy', 'mindy@ email.com');
+    hash.put(' Paul', 'paul@ email.com');
+    hash.put(' Nathan', 'nathan@ email.com');
+    hash.put('foo', 'felicia@mail.com');
+    hash.put('bar', 'brandon@mail.com');
+    hash.put('car', 'carina@mail.com');
+    hash.put('voo', 'vanessa@mail.com');
+    hash.put('jar', 'jessica@mail.com');
+    console.log("hash.get() = ");
+    console.log(hash.get(' Ana'));
+    console.log("uhhhhhhhhh, hello ????????");
+}
+// hashTableTest();
+testHash2();
 dash();
